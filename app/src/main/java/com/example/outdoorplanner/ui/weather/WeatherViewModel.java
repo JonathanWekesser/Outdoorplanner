caa16a5b3ed7953cbf872850460985e809c9ce50
jonathan <jw-213685@rwu.de>
2023-08-25 15:16:41 +0200

    Revert "zwischenstand"
    This reverts commit 9e4498f32cb945bc8864b91cd6152162e3d41a3a.

--- app/src/main/java/com/example/outdoorplanner/ui/weather/WeatherViewModel.java
+++ app/src/main/java/com/example/outdoorplanner/ui/weather/WeatherViewModel.java
@@ -1,15 +1,10 @@
 package com.example.outdoorplanner.ui.weather;
 
-import static java.security.AccessController.getContext;
-
-import android.content.Context;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.outdoorplanner.ui.weatherapi.RestInterface;
-import com.example.outdoorplanner.ui.weatherapi.WeatherAPI;
 
 public class WeatherViewModel extends ViewModel implements RestInterface {
     private final MutableLiveData<String> mText;
@@ -17,11 +12,6 @@ public class WeatherViewModel extends ViewModel implements RestInterface {
     private final MutableLiveData<String> currentTemp;
 
     public WeatherViewModel() {
-        WeatherAPI call;
-        call = new WeatherAPI();
-        call.registerRestEventListener(this);
-        // call.sendRequest(getContext());
-
         mText = new MutableLiveData<>();
         mText.setValue("This is weather fragment");
         location = new MutableLiveData<>();
@@ -39,6 +29,6 @@ public class WeatherViewModel extends ViewModel implements RestInterface {
 
     @Override
     public void onNewCurrentWeatherFromRest(String[] currentWeather) {
-        
+
     }
 }
